46190f946cb9d89a0bf4353606d7033bbca68b2e
Makayla <monkmonster96@gmail.com>
2018-10-02 18:37:51 -0400

    Added Separate Shader Files and Compile Error Checkers.

---
+++ test_fs.glsl
@@ -0,0 +1,5 @@
+  #version 410\n											// Fragment Shader for tree.
+  out vec4 frag_colour;
+  void main () {
+    frag_colour = vec4 (0.545, 0.27, 0.074, 1.0);    									//Makes tree brown.
+  }
